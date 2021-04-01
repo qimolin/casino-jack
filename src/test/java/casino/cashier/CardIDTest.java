e27505b5d2a6bfa1cc2f0298ae793bba63c4e5a3
slevakov <s.levakov@spinnov.com>
2021-04-01 18:26:09 +0200

    CardIdTest written

--- src/test/java/casino/cashier/CardIDTest.java
+++ src/test/java/casino/cashier/CardIDTest.java
@@ -2,6 +2,7 @@ package casino.cashier;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CardIDTest {
@@ -9,6 +10,9 @@ class CardIDTest {
     @Test
     public void CardID_shouldGenerateAndReturnAnID(){
         CardID sut = new CardID();
+        String id = sut.getID();
+        System.out.println(id);
+        assertThat(id).isNotNull();
     }
 
 }
