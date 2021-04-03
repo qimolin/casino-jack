57449891a0b44546f75e08628231a7a6d2d10b5f
Qi-Mo Lin <qimolin9@gmail.com>
2021-04-03 20:38:30 +0200

    Remove incorrect test

--- src/test/java/casino/game/BettingRoundTest.java
+++ src/test/java/casino/game/BettingRoundTest.java
@@ -13,19 +13,5 @@ import static org.mockito.Mockito.mock;
 
 public class BettingRoundTest {
 
-    /**
-     * @verifies BettingRound object is logically equal
-     */
-    @Test
-    public void BettingRound_WithSameBettingRoundIdAndBetTokenAreLogicallyEqual() {
-        // Arrange
-        GeneralID bettingRoundID = IDFactory.generateID("BETTINGROUNDID");
-        BetToken betToken = mock(BetToken.class);
-        IBettingRound A = new BettingRound((BettingRoundID) bettingRoundID, betToken);
-        IBettingRound B = new BettingRound((BettingRoundID) bettingRoundID, betToken);
-        // Act
-        // Assert
-        assertThat(A).isEqualTo(B);
-        assertThat(A).hasSameHashCodeAs(B);
-    }
+    
 }
