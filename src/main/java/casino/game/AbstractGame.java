b758d7cbd6d2bb2fa0240637d3334d913575c9c6
Qi-Mo Lin <qimolin9@gmail.com>
2021-04-03 22:51:56 +0200

    Impl(AbstractGame): constructor

--- src/main/java/casino/game/AbstractGame.java
+++ src/main/java/casino/game/AbstractGame.java
@@ -1,8 +1,33 @@
 package casino.game;
 
 
+import casino.cashier.ICashier;
 
 abstract class AbstractGame implements IGame{
 
- // define only the constructor here
+    private final ICashier cashier;
+    private IGameRule gameRule;
+
+    /**
+     * @should set cashier and gameRule
+     * @param cashier
+     * @param gameRule
+     */
+    public AbstractGame(ICashier cashier, IGameRule gameRule) {
+
+        this.cashier = cashier;
+        this.gameRule = gameRule;
+    }
+
+    public ICashier getCashier() {
+        return cashier;
+    }
+
+    public IGameRule getGameRule() {
+        return gameRule;
+    }
+
+    public void setGameRule(IGameRule gameRule) {
+        this.gameRule = gameRule;
+    }
 }
