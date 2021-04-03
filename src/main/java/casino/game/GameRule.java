53e8b29e35a922fc0950543e2d28c71bce3aa506
slevakov <s.levakov@spinnov.com>
2021-04-03 21:51:20 +0200

    GameRule tests added

--- src/main/java/casino/game/GameRule.java
+++ src/main/java/casino/game/GameRule.java
@@ -2,17 +2,34 @@ package casino.game;
 
 import casino.bet.Bet;
 import casino.bet.BetResult;
+import casino.bet.MoneyAmount;
 
 import java.util.Set;
 
 public class GameRule implements IGameRule{
+
+    private int maxBetsPerRound;
+
+    /**
+     * @should throw no bets exception
+     * @should return a random winner
+     * @param randomWinValue
+     * @param bets
+     * @return
+     * @throws NoBetsMadeException
+     */
     @Override
     public BetResult determineWinner(Integer randomWinValue, Set<Bet> bets) throws NoBetsMadeException {
-        return null;
+
+
     }
 
     @Override
     public int getMaxBetsPerRound() {
-        return 0;
+        return maxBetsPerRound;
+    }
+
+    public void setMaxBetsPerRound(int maxBetsPerRound) {
+        this.maxBetsPerRound = maxBetsPerRound;
     }
 }
